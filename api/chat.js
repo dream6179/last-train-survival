ee2465ae6bc17171618b165fd05f12ace03deb2a
dream6179 <zhtttttt@gmail.com>
2026-04-02 16:02:56 +0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -7,7 +7,8 @@ export default async function handler(req, res) {
     }
 
     const { prompt } = req.body;
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
+
 
     const payload = {
         contents: [{
